f6a80430314b66688742e8150ed3cf5b82180b18
Barnaby <barnab.why@gmail.com>
2020-05-22 22:19:36 +0100

    stuf

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
-const {app, BrowserWindow, Menu} = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
+
+if (require('electron-squirrel-startup')) return app.quit()
 
 const path = require('path')
 
 Menu.setApplicationMenu(null)
 
 let mainWindow
-function createWindow () {
+function createWindow() {
     mainWindow = new BrowserWindow({
         width: 920,
         height: 640,
-        icon: __dirname+"/assets/icon.ico",
+        icon: __dirname + '/assets/icon.ico',
         webPreferences: {
             nodeIntegration: true,
-            preload: path.join(__dirname, 'ui/preload.js')
+            preload: path.join(__dirname, 'ui/preload.js'),
         },
-        show: false
+        show: false,
     })
 
     mainWindow.loadFile('ui/index.html')
 
-    mainWindow.openDevTools();
+    //mainWindow.openDevTools();
 
     mainWindow.once('ready-to-show', () => {
         mainWindow.show()
 
         start()
-        mainWindow.webContents.send("ready", { text: `Wahtson v${Bot.version}` })
+        mainWindow.webContents.send('ready', { text: `Wahtson v${Bot.version}` })
     })
 }
 
@@ -46,9 +48,8 @@ const toml = require('toml')
 
 const Bot = require('wahtson')
 
-
-const configPath = path.join(__dirname,"../../../config.toml")
-const dbPath = path.join(__dirname,"../../../database.sqlite")
+const configPath = path.join(process.env.appdata,'wahtson-electron/Local Storage', 'config.toml')
+const dbPath = path.join(process.env.appdata,'wahtson-electron/Local Storage', 'database.sqlite')
 
 const bot = new Bot({
     dbPath,
@@ -56,11 +57,11 @@ const bot = new Bot({
 })
 
 bot.on('log', ({ level, text }) => {
-    mainWindow.webContents.send("log", { level, text })
+    mainWindow.webContents.send('log', { level, text })
 })
 
 bot.on('action', ({ index, numActions, action, skipped, source, event }) => {
-    mainWindow.webContents.send("action", { index, numActions, action, skipped, source, event })
+    mainWindow.webContents.send('action', { index, numActions, action, skipped, source, event })
 })
 
 const start = () => {
@@ -79,36 +80,45 @@ const start = () => {
                 configChangedLast = Date.now()
 
                 //console.log(chalk.grey('Config file changed, reloading...'))
-                mainWindow.webContents.send("log", { level: -1, text: 'Config file changed, reloading...' })
+                mainWindow.webContents.send('log', {
+                    level: -1,
+                    text: 'Config file changed, reloading...',
+                })
                 loadConfig(configPath)
                     .then(config => {
                         bot.config.reset(config)
                     })
                     .catch(err => {
                         //console.error(chalk.red(err))
-                        mainWindow.webContents.send("log", { level: 2, text: err })
+                        mainWindow.webContents.send('log', { level: 2, text: err })
                     })
             })
         })
         .catch(err => {
             //console.error(chalk.red(err))
-            mainWindow.webContents.send("log", { level: 2, text: err })
+            mainWindow.webContents.send('log', { level: 2, text: err })
         })
 }
 
 async function loadConfig(configPath) {
     return new Promise(async (resolve, reject) => {
-        if(!(await fs.exists(configPath))) {
-            await fs.copyFile(path.join(__dirname,"node_modules/wahtson/config-example.toml"), configPath)
+        if (!(await fs.exists(configPath))) {
+            await fs.copyFile(
+                path.join(__dirname, 'node_modules/wahtson/config-example.toml'),
+                configPath,
+            )
         }
-    
+
         const source = await fs.readFile(configPath, 'utf8')
 
         try {
             resolve(toml.parse(source))
         } catch (err) {
             reject()
-            mainWindow.webContents.send("log", { level: 2, text: `Syntax error in config on line ${err.line} column ${err.column}` })
+            mainWindow.webContents.send('log', {
+                level: 2,
+                text: `Syntax error in config on line ${err.line} column ${err.column}`,
+            })
         }
     })
 }
